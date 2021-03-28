eca01a58c45108e68dd2a7a4ec949d9a2701b833
stavramovic <stefan.avramovic096@gmail.com>
2021-03-29 01:35:57 +0200

    innit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss'
+import Coin from './Coin';
+import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [currency, setCurrency] = useState('usd');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency='+currency+'&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+      })
+      .catch(error => console.log(error));
+  }, [currency]);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'> 
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+          
+          <select className='currencyBar' 
+            value={currency}
+            onChange={(e) => {
+            const selectedCurrency = e.target.value;
+            setCurrency(selectedCurrency);
+          }}>
+            <option value='usd'>USD</option>
+            <option value='eur'>EUR</option>
+            <option value='gbp'>GBP</option>
+           </select>
+         
+        </form>
+      </div>
+      <Container className='coinInfo'>
+        <Row className='coinInfoPos'>
+          <Col md='12' className='infoData'>
+            <Col md='1' xs='1' className='pr-md-0 pr-5'></Col>
+            <Col md='1'  sm='5' xs='3' className='text-left' >Name</Col>
+            <Col md='2' sm=''  className='d-none d-sm-block'  ></Col>
+            <Col lg='2' md='3' sm='3' xs='5'  className='text-sm-right text-center pr-md-3 pr-sm-4 pr-4'> Price&nbsp;({currency})</Col>
+            <Col lg='2' md='3'  className='d-none d-md-block pl-lg-4 pl-md-5 '>Volume</Col>
+            <Col lg='2' md='3'  sm='2' xs='3' className='pr-lg-2 pr-md-2 pr-sm-4 pr-4 pr-5'    >24h</Col>
+            <Col lg='2'   className='d-none d-lg-block'>Market&nbsp;cap</Col>
+          </Col>
+        </Row>
+         </Container>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            volume={coin.total_volume}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+
+export default App;
+
+ 
+
+
+
+
+
+
+
---
+++ src/Coin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Coin.css'
+import {Container, Row, Col, Image} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Coin = ({ name, image, symbol, price, marketcap, volume, priceChange}) => {
+  return (
+    <div>
+    <Container className='coin-container'>
+      <Row className='coin-row'>
+        <Col  md='12' className='coin'>
+          <Col  md='1' xs ><Image src={image} alt='crypto'/></Col>
+          <Col lg='1' md='2' xs='3' >{name}</Col>
+          <Col lg='2' md='1'  className='coin-symbol d-none d-sm-block text-center'>{symbol}</Col>
+        
+        
+          <Col lg='2' md='3' sm='2' xs='5' className='text-sm-right text-center'>{price}</Col>
+          <Col lg='2' md='3'  className='text-right d-none d-md-block'>{volume.toLocaleString()}</Col>
+          {priceChange < 0 ? (
+            <Col lg='2' md='3' xs='2' className='red text-center'>{priceChange.toFixed(2)}%</Col>
+          ) : (<Col lg='2' md='3' xs='2' className='green text-center'>{priceChange.toFixed(2)}%</Col>)}
+          <Col lg='2'  className='d-none d-lg-block text-right'>{marketcap.toLocaleString()}
+          </Col>
+        </Col>
+      </Row>
+      </Container>
+      </div>
+  );
+};
+
+export default Coin;
+
+
+
